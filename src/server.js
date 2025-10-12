42765c1f0b6bac008f9c5d8f16b7191f7259ef6b
hidemi2008 <gabrielhidemi2@gmail.com>
2025-10-12 14:36:49 -0400

    Criacao do caminho user

--- src/server.js
+++ src/server.js
@@ -4,16 +4,26 @@ const app = express()
 
 app.use(express.json())
 
-const usuario = []
+const usuarios = []
 
 app.get("/", (req, res) => {
     res.send("Você está na página principal")
 })
 
-app.post("/user", (req,res) => {
-    console.log(req.body)
+app.post("/user", (req, res) => {
+    const {nome, idade} = req.body
+
+    usuarios.push({nome,idade})
+
+    console.log(usuarios)
 
     res.send("Você está na página principal usando post")
+
+    res.status(201).send("Usuário criado")
+})
+
+app.get("/user", (req,res) => {
+    res.status(200).json(usuarios)
 })
 
 app.listen(3333, () => console.log("Hello word!!!"))
